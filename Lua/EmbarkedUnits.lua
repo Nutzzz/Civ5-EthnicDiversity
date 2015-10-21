be4c1ccffc77f38a63e5e38992cd47b52286679a
Nutzzz <enussbaum@hotmail.com>
2015-10-21 14:31:55 -0700

    Update EmbarkedUnits.lua

--- Lua/EmbarkedUnits.lua
+++ Lua/EmbarkedUnits.lua
@@ -4,15 +4,37 @@
 	Lua for embarkation overrides
 	by Nutty (2015)
 
+Note: If Cultural Diversity is active, its embarkation settings are used instead.
+  http://forums.civfanatics.com/showthread.php?t=521664
+
+The eras defined below (iMidEra, iLateEra) are adjusted if the following mods are activated:
+* Prehistoric Era (including the extra eras added by version 0.3 [v14+])
+  http://forums.civfanatics.com/showthread.php?t=517912
+* Prehistoric Era Reborn
+  http://forums.civfanatics.com/showthread.php?t=548467
+* CCTP v4+
+  http://forums.civfanatics.com/forumdisplay.php?f=472
+
+ERAS:
+* 0 = Ancient (ERA_ANCIENT)
+* 1 = Classical (ERA_CLASSICAL)
+* 2 = Medieval (ERA_MEDIEVAL)
+* 3 = Renaissance (ERA_RENAISSANCE)
+* 4 = Industrial (ERA_INDUSTRIAL)
+* 5 = Modern (ERA_MODERN)
+* 6 [G+K/BNW] = Atomic (ERA_POSTMODERN)
+  6 [vanilla] = Future (ERA_FUTURE)
+* 7 [G+K/BNW] = Information (ERA_FUTURE)
+
 --]]
+
+local iMidEra = 2;	-- Era to switch to Galleons
+local iLateEra = 5;	-- Era to switch to Transports
 
 local strPreHistID = "7c63c188-045b-4955-bbea-6c685d119c86";
 local strPHERebornID = "8c63c188-045b-4955-bbea-6c685d119c86";
 local strCCTP_ID = "f29d178c-94fe-40a4-bd64-ccf4e4870f61";
 local strCulDivID = "31a31d1c-b9d7-45e1-842c-23232d66cd47";
-local bCulDivActive = false;
-local iMidEra = 2;
-local iLateEra = 5;
 
 function OnEraChanged(iEraID, iPlayerID)
 	local pPlayer = Players[iPlayerID];
@@ -76,29 +98,35 @@ function OnEraChanged(iEraID, iPlayerID)
 	Teams[iTeamID]:UpdateEmbarkGraphics();
 end
 
+function OnGameInitComplete()
+	local iPlayerID = Game.GetActivePlayer()
+	OnEraChanged(Players[iPlayer]:GetCurrentEra(), iPlayerID);
+end
+
+local bCulDivActive = false;
 for i, mod in pairs(Modding.GetActivatedMods()) do
-	if (mod.ID == strCCTP_ID) then
-		if (mod.Version >= 4) then
-			iMidEra = iMidEra + 1;
-			iLateEra = iLateEra + 1;
-		end
+	if (mod.ID == strCulDivID) then
+		bCulDivActive = true;
+		break
 	elseif (mod.ID == strPreHistID or mod.ID == strPHERebornID) then
 		if (mod.ID == strPreHistID and mod.Version >= 14) then
 			iMidEra = iMidEra + 4;
 			iLateEra = iLateEra + 4;
+			break
 		else
 			iMidEra = iMidEra + 1;
 			iLateEra = iLateEra + 1;
+			break
 		end
-	elseif (mod.ID == strCulDivID) then
-		bCulDivActive = true;
+	elseif (mod.ID == strCCTP_ID) then
+		if (mod.Version >= 4) then
+			iMidEra = iMidEra + 1;
+			iLateEra = iLateEra + 1;
+			break
+		end
 	end
 end
 if not (bCulDivActive) then
+	Events.SequenceGameInitComplete.Add(OnGameInitComplete);
 	Events.SerialEventEraChanged.Add(OnEraChanged);
 end
-
-function OnGameInitComplete(iPlayerID)
-	OnEraChanged(Players[iPlayerID]:GetCurrentEra(), Game.GetActivePlayer());
-end
-Events.SequenceGameInitComplete.Add(OnGameInitComplete);
