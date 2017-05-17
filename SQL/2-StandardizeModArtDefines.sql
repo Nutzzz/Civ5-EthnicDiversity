f36797c3213a2e85e2bb706a05637756a6f75c98
Nutzzz <enussbaum@hotmail.com>
2017-05-17 10:50:00 -0700

    A few bugfixes and a few additional models
    * Add a few more of JTitan's ancient units
    * A couple of the new PE units were not VFS=true
    * Still having invisible/spearman issues with a couple of PE/EE units
    (but no crashing with FW after removing the culprits)

--- SQL/2-StandardizeModArtDefines.sql
+++ SQL/2-StandardizeModArtDefines.sql
@@ -9,6 +9,25 @@
 */
 
 --Prehistoric Era
+INSERT OR REPLACE INTO ArtDefine_UnitInfoMemberInfos (UnitInfoType, UnitMemberInfoType, NumMembers) 
+	SELECT 'ART_DEF_UNIT_CAVEMAN', 'ART_DEF_UNIT_MEMBER_CAVEMAN', NumMembers 
+	FROM ArtDefine_UnitInfoMemberInfos WHERE UnitInfoType = 'ART_DEF_UNIT_BARBARIAN_EURO';
+INSERT OR REPLACE INTO ArtDefine_UnitMemberInfos (Type, Scale, ZOffset, Domain, Model, MaterialTypeTag, MaterialTypeSoundOverrideTag)
+	SELECT Type || 'ART_DEF_UNIT_MEMBER_CAVEMAN', Scale, ZOffset, Domain, Model, MaterialTypeTag, MaterialTypeSoundOverrideTag
+	FROM ArtDefine_UnitMemberInfos WHERE Type = 'ART_DEF_UNIT_MEMBER_BARBARIAN_EURO';
+INSERT OR REPLACE INTO ArtDefine_UnitMemberCombats (UnitMemberType, EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation)
+	SELECT 'ART_DEF_UNIT_MEMBER_CAVEMAN', EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation
+	FROM ArtDefine_UnitMemberCombats WHERE UnitMemberType = 'ART_DEF_UNIT_MEMBER_BARBARIAN_EURO';
+INSERT OR REPLACE INTO ArtDefine_UnitMemberCombatWeapons (UnitMemberType, "Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag)
+	SELECT 'ART_DEF_UNIT_MEMBER_CAVEMAN', "Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag
+	FROM ArtDefine_UnitMemberCombatWeapons WHERE UnitMemberType = 'ART_DEF_UNIT_MEMBER_BARBARIAN_EURO';
+INSERT OR REPLACE INTO ArtDefine_UnitInfos (Type, DamageStates, Formation, UnitFlagAtlas, UnitFlagIconOffset, IconAtlas, PortraitIndex)
+	SELECT 'ART_DEF_UNIT_CAVEMAN', DamageStates, Formation, UnitFlagAtlas, UnitFlagIconOffset, IconAtlas, PortraitIndex
+	FROM ArtDefine_UnitInfos WHERE Type = 'ART_DEF_UNIT_BARBARIAN_EURO';
+INSERT OR REPLACE INTO ArtDefine_StrategicView (StrategicViewType, TileType, Asset)
+	SELECT 'ART_DEF_UNIT_CAVEMAN', TileType, 'Caveman_sv.dds' 
+	FROM ArtDefine_StrategicView WHERE StrategicViewType = 'ART_DEF_UNIT_BARBARIAN_EURO';
+
 INSERT INTO ArtDefine_UnitInfoMemberInfos (UnitInfoType, UnitMemberInfoType, NumMembers)
 	SELECT UnitInfoType || '_ED', UnitMemberInfoType || '_ED', NumMembers
 	FROM ArtDefine_UnitInfoMemberInfos WHERE UnitInfoType IN 
@@ -75,16 +94,35 @@ INSERT OR REPLACE INTO ArtDefine_StrategicView (StrategicViewType, TileType, Ass
 
 --Enlightenment Era
 INSERT OR REPLACE INTO ArtDefine_UnitInfoMemberInfos (UnitInfoType, UnitMemberInfoType, NumMembers) 
+	SELECT 'ART_DEF_UNIT_EE_GALLEON', 'ART_DEF_UNIT_MEMBER_EE_GALLEON', NumMembers 
+	FROM ArtDefine_UnitInfoMemberInfos WHERE UnitInfoType = 'ART_DEF_UNIT_U_SPANISH_GALLEON';
+INSERT OR REPLACE INTO ArtDefine_UnitMemberInfos (Type, Scale, ZOffset, Domain, Model, MaterialTypeTag, MaterialTypeSoundOverrideTag)
+	SELECT Type || 'ART_DEF_UNIT_MEMBER_EE_GALLEON', Scale, ZOffset, Domain, Model, MaterialTypeTag, MaterialTypeSoundOverrideTag
+	FROM ArtDefine_UnitMemberInfos WHERE Type = 'ART_DEF_UNIT_MEMBER_U_SPANISH_GALLEON';
+INSERT OR REPLACE INTO ArtDefine_UnitMemberCombats (UnitMemberType, EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation)
+	SELECT 'ART_DEF_UNIT_MEMBER_EE_GALLEON', EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation
+	FROM ArtDefine_UnitMemberCombats WHERE UnitMemberType = 'ART_DEF_UNIT_MEMBER_U_SPANISH_GALLEON';
+INSERT OR REPLACE INTO ArtDefine_UnitMemberCombatWeapons (UnitMemberType, "Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag)
+	SELECT 'ART_DEF_UNIT_MEMBER_EE_GALLEON', "Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag
+	FROM ArtDefine_UnitMemberCombatWeapons WHERE UnitMemberType = 'ART_DEF_UNIT_MEMBER_U_SPANISH_GALLEON';
+INSERT OR REPLACE INTO ArtDefine_UnitInfos (Type, DamageStates, Formation, UnitFlagAtlas, UnitFlagIconOffset, IconAtlas, PortraitIndex)
+	SELECT 'ART_DEF_UNIT_EE_GALLEON', DamageStates, Formation, UnitFlagAtlas, UnitFlagIconOffset, IconAtlas, PortraitIndex
+	FROM ArtDefine_UnitInfos WHERE Type = 'ART_DEF_UNIT_U_SPANISH_GALLEON';
+INSERT OR REPLACE INTO ArtDefine_StrategicView (StrategicViewType, TileType, Asset)
+	SELECT 'ART_DEF_UNIT_EE_GALLEON', TileType, Asset
+	FROM ArtDefine_StrategicView WHERE StrategicViewType = 'ART_DEF_UNIT_U_SPANISH_GALLEON';
+
+INSERT OR REPLACE INTO ArtDefine_UnitInfoMemberInfos (UnitInfoType, UnitMemberInfoType, NumMembers) 
 	SELECT UnitInfoType || '_ED', UnitMemberInfoType || '_ED', NumMembers 
 	FROM ArtDefine_UnitInfoMemberInfos WHERE UnitInfoType IN 
-	('ART_DEF_UNIT_EE_CARRACK', 'ART_DEF_UNIT_EE_CUIRASSIER', 'ART_DEF_UNIT_EE_FIELD_GUN', 
+	('ART_DEF_UNIT_EE_CARRACK', 'ART_DEF_UNIT_EE_CUIRASSIER', --'ART_DEF_UNIT_EE_FIELD_GUN', 
 	'ART_DEF_UNIT_EE_GALLEON', 'ART_DEF_UNIT_EE_LINE_INFANTRY', 'ART_DEF_UNIT_EE_SKIRMISHER');
 INSERT OR REPLACE INTO ArtDefine_UnitMemberInfos (Type, Scale, ZOffset, Domain, Model, MaterialTypeTag, MaterialTypeSoundOverrideTag)
 	SELECT Type || '_ED', Scale, ZOffset, Domain, Model, MaterialTypeTag, MaterialTypeSoundOverrideTag
 	FROM ArtDefine_UnitMemberInfos WHERE 
 	(Type LIKE 'ART_DEF_UNIT_MEMBER_EE_CARRACK%' OR 
 	Type LIKE 'ART_DEF_UNIT_MEMBER_EE_CUIRASSIER%' OR 
-	Type LIKE 'ART_DEF_UNIT_MEMBER_EE_FIELD_GUN%' OR 
+	--Type LIKE 'ART_DEF_UNIT_MEMBER_EE_FIELD_GUN%' OR 
 	Type LIKE 'ART_DEF_UNIT_MEMBER_EE_GALLEON%' OR 
 	Type LIKE 'ART_DEF_UNIT_MEMBER_EE_LINE_INFANTRY%' OR 
 	Type LIKE 'ART_DEF_UNIT_MEMBER_EE_SKIRMISHER%');
@@ -93,8 +131,8 @@ INSERT OR REPLACE INTO ArtDefine_UnitMemberCombats (UnitMemberType, EnableAction
 	FROM ArtDefine_UnitMemberCombats WHERE 
 	(UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_CARRACK%' OR 
 	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_CUIRASSIER%' OR 
-	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_FIELD_GUN%' OR 
-	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_GALLEON%' OR 
+	--UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_FIELD_GUN%' OR 
+	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_GALLEON%' OR
 	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_LINE_INFANTRY%' OR 
 	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_SKIRMISHER%');
 INSERT OR REPLACE INTO ArtDefine_UnitMemberCombatWeapons (UnitMemberType, "Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag)
@@ -102,21 +140,20 @@ INSERT OR REPLACE INTO ArtDefine_UnitMemberCombatWeapons (UnitMemberType, "Index
 	FROM ArtDefine_UnitMemberCombatWeapons WHERE 
 	(UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_CARRACK%' OR 
 	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_CUIRASSIER%' OR 
-	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_FIELD_GUN%' OR 
-	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_GALLEON%' OR 
+	--UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_FIELD_GUN%' OR 
+	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_GALLEON%' OR
 	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_LINE_INFANTRY%' OR 
 	UnitMemberType LIKE 'ART_DEF_UNIT_MEMBER_EE_SKIRMISHER%');
 INSERT OR REPLACE INTO ArtDefine_UnitInfos (Type, DamageStates, Formation, UnitFlagAtlas, UnitFlagIconOffset, IconAtlas, PortraitIndex)
 	SELECT Type || '_ED', DamageStates, Formation, UnitFlagAtlas, UnitFlagIconOffset, IconAtlas, PortraitIndex
 	FROM ArtDefine_UnitInfos WHERE Type IN 
-	('ART_DEF_UNIT_EE_CARRACK', 'ART_DEF_UNIT_EE_CUIRASSIER', 'ART_DEF_UNIT_EE_FIELD_GUN', 
+	('ART_DEF_UNIT_EE_CARRACK', 'ART_DEF_UNIT_EE_CUIRASSIER', --'ART_DEF_UNIT_EE_FIELD_GUN', 
 	'ART_DEF_UNIT_EE_GALLEON', 'ART_DEF_UNIT_EE_LINE_INFANTRY', 'ART_DEF_UNIT_EE_SKIRMISHER');
 INSERT OR REPLACE INTO ArtDefine_StrategicView (StrategicViewType, TileType, Asset)
 	SELECT StrategicViewType || '_ED', TileType, Asset
 	FROM ArtDefine_StrategicView WHERE StrategicViewType IN 
-	('ART_DEF_UNIT_EE_CARRACK', 'ART_DEF_UNIT_EE_CUIRASSIER', 'ART_DEF_UNIT_EE_FIELD_GUN', 
+	('ART_DEF_UNIT_EE_CARRACK', 'ART_DEF_UNIT_EE_CUIRASSIER', --'ART_DEF_UNIT_EE_FIELD_GUN', 
 	'ART_DEF_UNIT_EE_GALLEON', 'ART_DEF_UNIT_EE_LINE_INFANTRY', 'ART_DEF_UNIT_EE_SKIRMISHER');
-	--FIELD_GUN removed
 
 --Future Worlds
 INSERT OR REPLACE INTO ArtDefine_UnitInfoMemberInfos (UnitInfoType, UnitMemberInfoType, NumMembers) 
--- SQL/8-ModUnitPack.sql
+++ SQL/8-ModUnitPack.sql
@@ -10,20 +10,29 @@
 
 --Prehistoric Era
 UPDATE Units SET UnitArtInfo = UnitArtInfo || '_ED' WHERE Type IN 
-	('UNIT_AUTOCHTHON', 'UNIT_CAVEMAN', 'UNIT_EXPLORER', 'UNIT_HUNTER', 'UNIT_SAVAGE');
+	('UNIT_AUTOCHTHON', 'UNIT_HUNTER', 'UNIT_SAVAGE');
+UPDATE Units SET UnitArtInfo = 'ART_DEF_UNIT_CAVEMAN_ED' WHERE Type = 'UNIT_CAVEMAN';
+UPDATE Units SET UnitArtInfo = 'ART_DEF_UNIT_EXPLORER_PE' 
+	WHERE Type = 'UNIT_EXPLORER' AND MoveRate = 'QUADRUPED';
+
 --Renaissance Era Revised
 UPDATE Units SET UnitArtInfo = 'ART_DEF_UNIT_2HANDER_ED' WHERE Type = 'UNIT_2HANDER';
+
 --Enlightenment Era
 UPDATE Units SET UnitArtInfo = UnitArtInfo || '_ED' WHERE Type IN 
 	('UNIT_EE_CARRACK', 'UNIT_EE_CUIRASSIER', 'UNIT_EE_LINE_INFANTRY', 'UNIT_EE_SKIRMISHER'); --'UNIT_EE_FIELD_GUN'
 UPDATE Units SET UnitArtInfo = 'ART_DEF_UNIT_EE_GALLEON_ED' WHERE Type = 'UNIT_EE_GALLEON';
+
 --Future Worlds
 UPDATE Units SET UnitArtInfo = UnitArtInfo || '_ED' WHERE Type IN 
 	('UNIT_FUTURE_SOLDIER', 'UNIT_FW_HOVERTANK', --'UNIT_FW_BIOTROOPER', 'UNIT_FW_DRONE_FIGHTER_2', 'UNIT_FW_HYBRID_DRONE'
 	'UNIT_FW_NANOHIVE', 'UNIT_FW_RAILGUN_TANK', 'UNIT_FW_SUPERCARRIER', 'UNIT_FW_VERTOL');
+
 --Vox Populi
 UPDATE Units SET UnitArtInfo = UnitArtInfo || '_ED' 
 	WHERE Type = 'UNIT_ARMORED_CAR';
+--UPDATE Units SET UnitArtInfo = 'ART_DEF_UNIT_EXPLORER_CBP' 
+--	WHERE Type = 'UNIT_EXPLORER' AND MoveRate = 'BIPED';
 
 --Misc:
 --"Units - Super Carrier" by whoward69 and "Supercarrier" by swatmp5
